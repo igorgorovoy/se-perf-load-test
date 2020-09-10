bd556956b86015b6e718e83c0851c6ac33ec68b5
Igor Gorovoy <igorgorovoy@ukr.net>
2020-09-10 19:44:36 +0300

    add epoch time

--- test-scripts/scenario1_t2.sh
+++ test-scripts/scenario1_t2.sh
@@ -1,1 +1,1 @@
-curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min1hop.txt
+echo `date '+%s'"|" curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min1hop.txt
