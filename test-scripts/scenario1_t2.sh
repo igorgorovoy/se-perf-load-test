31ba6f1ed8ab1da39ad9ee5d7502e6c369cfb660
Igor Gorovoy <igorgorovoy@ukr.net>
2020-09-10 20:04:22 +0300

    fix call command

--- test-scripts/scenario1_t2.sh
+++ test-scripts/scenario1_t2.sh
@@ -1,1 +1,1 @@
-echo `date '+%s'"|" curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min1hop.txt
+echo `date '+%s'``"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min1hop.txt
--- test-scripts/scenario2_t1.sh
+++ test-scripts/scenario2_t1.sh
@@ -1,10 +1,10 @@
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt &
+echo `date '+%s'"|"`curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt &
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
+echo `date '+%s'"|" `curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
+echo `date '+%s'`"|" `curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
+echo `date '+%s'`"|" `curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
+echo `date '+%s'"|" `curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min5hop.txt&
