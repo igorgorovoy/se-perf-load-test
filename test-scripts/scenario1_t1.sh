dc375a7e65bdc82c18e267a78a9f15a4dcf468bf
Igor Gorovoy <igorgorovoy@ukr.net>
2020-09-10 20:12:18 +0300

    fix call command

--- test-scripts/scenario1_t1.sh
+++ test-scripts/scenario1_t1.sh
@@ -1,1 +1,1 @@
-echo `date '+%s'``"|"`curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min1hop.txt
+echo `date '+%s'`"|"`curl -so /dev/null https://stoic-jackson-4a8b51.netlify.app` >> /home/ubuntu/stoic-jackson-4a8b51.netlify.app-1min1hop.txt
--- test-scripts/scenario2_t2.sh
+++ test-scripts/scenario2_t2.sh
@@ -1,10 +1,10 @@
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" `curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
+echo `date '+%s'`"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" `curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
+echo `date '+%s'`"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" `curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
+echo `date '+%s'`"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" `curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
+echo `date '+%s'`"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
 sudo systemd-resolve --flush-caches &&
-echo `date '+%s'"|" `curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
+echo `date '+%s'`"|"`curl -so /dev/null https://master.d2a8dehuglpvfd.amplifyapp.com` >> /home/ubuntu/master.d2a8dehuglpvfd.amplifyapp.com-1min5hop.txt&
